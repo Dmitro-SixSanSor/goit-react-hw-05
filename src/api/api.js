434ccae32d47b5445e932acce524114efe000825
Dmitro-SixSanSor <dmytro.ostaphiychuk@gmail.com>
2025-06-01 22:14:03 +0300

    add markup header

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
-const API_KEY = "f5fc0e81355eaddcbaa48778e3559d5c";
+const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNWZjMGU4MTM1NWVhZGRjYmFhNDg3NzhlMzU1OWQ1YyIsIm5iZiI6MTc0ODgwNDI2MC4xODcsInN1YiI6IjY4M2NhMmE0ZGQ2ODYyNGY0OWFkN2JmZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.v1D-mrFWjnCIEnH8l388Sxjn3oP_KHii-tSE_g_g40Y";
 
 export const fetchMovies = async (value) => {
   const fetchMovieSearch = await axios.get('/search/movie', {
